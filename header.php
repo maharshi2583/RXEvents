420ad3a60435af19d6d5aaa3ce9feed7e61ccf7d
Maharshi Pithava <maharshi.pithava.ordex@gmail.com>
2025-01-12 14:20:17 +0530

    header working

---
+++ header.php
@@ -0,0 +1,64 @@
+<header class="container-fluid py-2" id="subHeader">
+    <div class="row px-2 px-md-5">
+        <div class="col-md-6">
+            <div class="d-flex">
+                <a href="" class="mx-2 fs-5 social-links"><i class="fa-brands fa-square-facebook"></i></a>
+                <a href="" class="mx-2 fs-5 social-links"><i class="fa-brands fa-square-x-twitter"></i></a>
+                <a href="" class="mx-2 fs-5 social-links"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="" class="mx-2 fs-5 social-links"><i class="fa-brands fa-square-instagram"></i></a>
+            </div>
+        </div>
+        <div class="col-md-6 d-flex align-items-center justify-content-end">
+            <a href="" class="me-4 text-decoration-none social-links white-underline">Image Consulting</a>
+            <a href="" class="text-decoration-none social-links white-underline">Rx-Tv</a>
+        </div>
+
+    </div>
+</header>
+
+
+<nav class="navbar navbar-expand-xl bg-white py-0">
+    <div class="container-fluid px-4">
+        <a class="navbar-brand" href="#"><img src="images/rxTransparentLogo.png" class="py-0" height="80"
+                alt="logo"></a>
+        <button class="navbar-toggler order-1 order-xl-0 position-relative" type="button" data-bs-toggle="collapse"
+            data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <input type="checkbox" name="" id="">
+            <div class="hamburger-lines">
+                <span class="line line1"></span>
+                <span class="line line2"></span>
+                <span class="line line3"></span>
+            </div>
+        </button>
+        <div class="collapse navbar-collapse order-1 order-xl-0" id="mainNavbar">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-xl-center">
+                <li class="nav-item mx-lg-2">
+                    <a class="nav-link text-uppercase" href="index.php">Home</a>
+                </li>
+                <li class="nav-item mx-lg-2">
+                    <a class="nav-link text-uppercase" href="aboutus.php">About Us</a>
+                </li>
+                <li class="nav-item mx-lg-2">
+                    <a class="nav-link text-uppercase" href="committee.php">Services</a>
+                </li>
+                <li class="nav-item mx-lg-2">
+                    <a class="nav-link text-uppercase" href="internationalFaculties.php">Gallery</a>
+                </li>
+                <li class="nav-item mx-lg-2">
+                    <a class="nav-link text-uppercase" href="sponsorship.php">Events</a>
+                </li>
+                <li class="nav-item mx-lg-2">
+                    <a class="nav-link text-uppercase" href="scientific.php">Resources</a>
+                </li>
+                <li class="nav-item mx-lg-2">
+                    <a class="nav-link text-uppercase" href="accommodation.php">Contact Us</a>
+                </li>
+                <li class="nav-item mx-lg-2">
+                    <a class="nav-link text-uppercase btn btn-main px-3 bookADemo" href="contactus.php">Book
+                        A Demo</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
